751f332d90e7899188d199b90368dac36ca5ad91
it21047138 <it21047138@my.sliit.lk>
2022-11-04 22:39:07 +0530

    final project

---
+++ src/main/java/com/example/helthcarehospital/addnote.java
@@ -0,0 +1,51 @@
+package com.example.helthcarehospital;
+
+public class addnote {
+
+
+    private String NoteNo;
+    private String Date;
+    private String Description;
+
+
+    public String getNoteNo() {
+        return NoteNo;
+    }
+
+    public void setNoteNo(String noteNo) {
+        NoteNo = noteNo;
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public void setDate(String date) {
+        Date = date;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public addnote() {
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/main/java/com/example/helthcarehospital/viewappoinments.java
@@ -0,0 +1,14 @@
+package com.example.helthcarehospital;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class viewappoinments extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_viewappoinments);
+    }
+}
